0c95686693ee44ecaeb66b15eb2ad6687567bcc6
VasilyGorbunov <vasily.gorbunov#gmail.com>
2022-11-06 23:08:59 +0300

    lesson 10

--- app/Http/Requests/Product/StoreRequest.php
+++ app/Http/Requests/Product/StoreRequest.php
@@ -13,7 +13,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,16 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'preview_image' => 'required',
+            'price' => 'required',
+            'count' => 'required',
+            'is_published' => 'nullable',
+            'category_id' => 'nullable',
+            'tags' => 'nullable|array',
+            'colors' => 'nullable|array',
         ];
     }
 }
